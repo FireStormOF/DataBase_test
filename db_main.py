663b4b373e73a2f2aca08ca09058134b09e9b735
Ivan Yashchuk <ivanko3224@gmail.com>
2023-01-05 19:17:58 +0200

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -1,3 +1,29 @@
-print(1)
-print("sisisisiisisi")
-print('ya ebal hp')
+import pymysql
+
+def bd_connect(host,user,password,database):
+    try:
+        # connect to exist database
+        connection = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database   
+        )
+        connection.autocommit = True
+        
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT version();"
+            )
+            
+            print(f"Server version: {cursor.fetchone()}")
+            
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            # cursor.close()
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
+
+# bd_connect("localhost","root","123Qwerty123","Test_bd")
