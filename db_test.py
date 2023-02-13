94758850b7d7bac88dd7b6ca48458d90723a8c96
Ivan Yashchuk <ivanko3224@gmail.com>
2023-02-13 16:34:16 +0200

    2223

--- db_test.py
+++ db_test.py
@@ -3,25 +3,13 @@ try:
     con = sqlite3.connect('D:\Proj_main\main.db')
     cursor = con.cursor()
     print("Successfully Connected to SQLite")
-    # name = "MAX"
-    # data_insert = f"""
-    # INSERT INTO validUser
-    #                       (name) 
-    #                        VALUES 
-    #                       ("{name}")
-    
-    # """
-    Telegram_id = 234235235
-    name = "name"
-    lastname = "lname"
-    patronymic = "pat"
-    course = "1"
-    spec = "cs-15"
-    band = "gr"
-    sql = f"""INSERT INTO validUser(Telegram_id, name, lastname, patronymic, course, spec) \
-                                            VALUES("{Telegram_id}", "{name}", "{lastname}", "{patronymic}", "{course}", "{spec}")"""
-    cursor.execute(sql)
-    con.commit()
 
+    id_select = """
+    select Telegram_id from validUser
+    """
+    cursor.execute(id_select)
+    con.commit()
+    list  = cursor.fetchall()
+    print(list)
 except sqlite3.Error as error:
     print("Failed to insert data into sqlite table", error)
